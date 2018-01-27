4a8db67ab26ca75a66515649aafbcb1bf19ce836
VAIBHAV SOLANKI <vbhvsolanki7500@gmail.com>
2018-01-27 19:23:14 +0530

    Add files via upload

---
+++ Form1.vb
@@ -0,0 +1,21 @@
+﻿Imports System.Speech
+Imports System.Speech.Synthesis
+
+
+
+Public Class Form1
+
+    Dim Speaker As New SpeechSynthesizer
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Speaker.Dispose()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Speaker.SpeakAsync(RichTextBox1.Text)
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Speaker.SpeakAsyncCancelAll()
+    End Sub
+End Class
